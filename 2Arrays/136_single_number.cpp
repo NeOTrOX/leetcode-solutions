a353d672386c066f88d63070b8b6693ad525998b
Divyansh Choudhary <divyanshchoudhary22rj@gmail.com>
2025-09-18 12:16:47 +0530

    Single Number

--- 2Arrays/136_single_number.cpp
+++ 2Arrays/136_single_number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xor1 = 0;
-        for(int i = 0; i<nums.size(); i++){
+        int xor1 = 0;   // Initialize result
+
+        // XOR all elements
+        for (int i = 0; i < nums.size(); i++) {
             xor1 = xor1 ^ nums[i];
         }
-        return xor1;
+
+        return xor1;    // The remaining number is the single one
     }
 };
