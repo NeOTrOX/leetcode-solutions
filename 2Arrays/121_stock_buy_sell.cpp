6a088159cad420e6efb47d0de355f7a636d85ef1
Divyansh Choudhary <divyanshchoudhary22rj@gmail.com>
2025-09-18 12:16:04 +0530

    Stock Buy Sell

--- 2Arrays/121_stock_buy_sell.cpp
+++ 2Arrays/121_stock_buy_sell.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int mini = prices[0];
-        int maxProfit = 0;
+        int mini = prices[0];       // Store the minimum price so far (best buy point)
+        int maxProfit = 0;          // Store maximum profit found so far
         int n = prices.size();
-        for(int i = 1; i<n; i++){
-            int cost = prices[i] - mini;
-            maxProfit = max(maxProfit, cost);
-            mini = min(mini, prices[i]);
+
+        for (int i = 1; i < n; i++) {
+            int cost = prices[i] - mini;          // Profit if we sell today
+            maxProfit = max(maxProfit, cost);     // Update maximum profit
+            mini = min(mini, prices[i]);          // Update minimum price (buy cheaper if possible)
         }
-        return maxProfit;
+
+        return maxProfit;   // Return the best profit possible
     }
 };
