b79af3b0a8ab9625e41cd82d48ed659fadfba6da
Divyansh Choudhary <divyanshchoudhary22rj@gmail.com>
2025-09-18 12:22:47 +0530

    Subarray Sum Equals K

--- 2Arrays/560_subarray_sum_equals_k.cpp
+++ 2Arrays/560_subarray_sum_equals_k.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-    unordered_map<int,int> mpp;
-    mpp[0] = 1;
-    int preSum = 0, cnt = 0;
+        unordered_map<int,int> mpp;  // stores frequency of prefix sums
+        mpp[0] = 1;                  // base case: prefix sum 0 occurs once
+        int preSum = 0;               // current prefix sum
+        int cnt = 0;                  // count of subarrays with sum = k
 
-    for(int i = 0; i < nums.size(); i++){
-        preSum += nums[i];
-        int remove = preSum - k;
-        cnt += mpp[remove];
-        mpp[preSum] += 1;
-    }
-    return cnt;        
+        for (int i = 0; i < nums.size(); i++) {
+            preSum += nums[i];             // update prefix sum
+            int remove = preSum - k;       // check if there exists a prefix sum to remove
+            cnt += mpp[remove];            // add frequency of that prefix sum
+            mpp[preSum] += 1;              // update frequency of current prefix sum
+        }
+
+        return cnt;        
     }
 };
