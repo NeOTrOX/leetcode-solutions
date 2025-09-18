9526a8306bc46c33263c6b6d0ea8c0a13f79ddd0
Divyansh Choudhary <divyanshchoudhary22rj@gmail.com>
2025-09-18 12:21:58 +0530

    Max Consecutive Zeroes

--- 2Arrays/485_max_consecutive_ones.cpp
+++ 2Arrays/485_max_consecutive_ones.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int maxi = 0;
-        int count = 0;
+        int maxi = 0;   // stores maximum consecutive 1s
+        int count = 0;  // current streak of 1s
 
-        for(int i=0; i < nums.size(); i++){
-            if(nums[i] == 1){
-                count++;
-                maxi = max(maxi, count);
-            }
-            else {
-                count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                count++;                    // increase streak
+                maxi = max(maxi, count);    // update answer
+            } else {
+                count = 0;  // reset streak when we hit a 0
             }
         }
+
         return maxi;
     }
 };
