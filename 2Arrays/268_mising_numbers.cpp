bb9b401e56afe771e088794e515b4e647eb8e907
Divyansh Choudhary <divyanshchoudhary22rj@gmail.com>
2025-09-18 12:19:37 +0530

    Missing Numbers

--- 2Arrays/268_mising_numbers.cpp
+++ 2Arrays/268_mising_numbers.cpp
@@ -2,11 +2,17 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
+
+        // Total sum of numbers from 0 to n (formula of sum of first n numbers)
         int total = n * (n + 1) / 2;
+
+        // Sum of given numbers
         int sum = 0;
         for (int num : nums) {
             sum += num;
         }
+
+        // Missing number = total expected - actual sum
         return total - sum;
     }
 };
